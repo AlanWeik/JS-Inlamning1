cf6f124490cc8da7f0146b84e9e15ca857ea53cc
Alan Weik <alaan.m@hotmail.com>
2021-02-03 13:18:00 +0100

    Re-wrote all the JS. Deleted old file and made a new one.

--- stuff.js
+++ stuff.js
@@ -1,59 +1,61 @@
 document.addEventListener("DOMContentLoaded", onPageLoad);
 
+
 function onPageLoad() 
 {
-  let inputBox = document.querySelector("#input-number");
+    let inputBox = document.querySelector("#input-number")
 
-  inputBox.oninput = function() 
-  {
-    removeAllSections();
-    makeSections(parseInt(inputBox.value));
-  };
+    inputBox.oninput = function() 
+    {
+        removeAllSections();
+        makeSections(parseInt(inputBox.value));
+    };
 }
 
 function makeSections(count)
 {
-  for (var i = 0; i < count; i++) 
-  {
-    var parent = document.querySelector("main");
+    for (var i = 0; i < count; i++) 
+    {
+        var parent = document.querySelector("main");
+        
+        var child = document.createElement("section");
+        var title = document.createElement("h4");
+        var para = document.createElement("p");
 
-    var child = document.createElement("section");
-    var title = document.createElement("h4");
-    var para = document.createElement("p");
+        title.innerText = "Title " + i;
+        para.innerText = `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Inventore harum, quasi dicta ex totam quisquam quo tempore maxime, 
+        commodi praesentium eius quod suscipit! 
+        Tenetur magnam eligendi amet fugiat adipisci impedit.`;
 
-    title.innerText = "Title " + i;
-    para.innerText = `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-      Inventore harum, quasi dicta ex totam quisquam quo tempore maxime, 
-      commodi praesentium eius quod suscipit! 
-      Tenetur magnam eligendi amet fugiat adipisci impedit.`;
+        makeEditable(title);
+        makeEditable(para);
 
-    makeEditable(title);
-    makeEditable(para);
+        child.append(title);
+        child.append(para);
+        parent.append(child);
 
-    child.append(title);
-    child.append(para);
-    parent.append(child);
-  }
+    }
 }
 
 function removeAllSections()
 {
-  var test_sections = document.querySelectorAll("section");
-  for (var i = 0; i < test_sections.length; i++) 
-  {
-    test_sections[i].remove();
-  }
+    var test_sections = document.querySelectorAll("section");
+    for (var i = 0; i < test_sections.length; i++)
+    {
+        test_sections[i].remove();
+    }
 }
 
 function makeEditable(elem)
 {
-  elem.onclick = function(e) 
-  {
-    elem.contentEditable = true;
-    elem.focus();
-  };
-  elem.onblur = function(e) 
-  {
-    elem.contentEditable = false;
-  };
+    elem.onclick = function(e)
+    {
+        elem.contentEditable = true;
+        elem.focus();
+    };
+    elem.onblur = function(e)
+    {
+        elem.contentEditable = false;
+    };
 }
